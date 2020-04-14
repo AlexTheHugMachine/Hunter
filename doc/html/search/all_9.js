835c9c58703797518019ded9c25c5aef1f85e111
1803588 <lucas.rakotomalala@etu.univ-lyon1.fr>
2020-04-14 11:50:39 +0200

    init

--- doc/html/search/all_9.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['menu',['Menu',['../classMenu.html',1,'']]],
-  ['menu_5fsdl',['menu_sdl',['../classmenu__sdl.html',1,'']]],
-  ['move',['move',['../classPlayer.html#a1aa6d0f302c6d6992ac0a993d2530e36',1,'Player']]]
-];
--- doc/html/search/variables_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['coords',['coords',['../classNode.html#a141b294027592dff7eb8764a3a94ccfb',1,'Node']]]
-];
